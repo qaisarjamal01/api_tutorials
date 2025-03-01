0e3ad0d9b9b4b64e39e62f0f1d2e1bb17ca8c513
qaisarjamal01 <qaisarjamaluop@gmail.com>
2025-03-01 16:48:23 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:api_tutorial/Again/SharedPreferences/shared_preferes.dart';
 import 'package:api_tutorial/Again/SharedPreferences/splash_screen.dart';
 import 'package:api_tutorial/Again/ripple_animation.dart';
 import 'package:api_tutorial/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
